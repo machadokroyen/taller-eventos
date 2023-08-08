7c648bd507315d995d3e78ea47ed7fac032cc3a4
Mauricio Machado <133698730+Kroyen@users.noreply.github.com>
2023-08-08 16:07:46 -0300

    Alerta Div

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let conte = document.querySelector("div");
+    conte.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+})
