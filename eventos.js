f6d96ad4d63ef0dff6568e05439eb9088d94e3cb
Mauricio Machado <133698730+machadokroyen@users.noreply.github.com>
2023-08-08 21:44:26 -0300

    version 1.3

--- eventos.js
+++ eventos.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
     let conte = document.querySelector("div");
+    let boton = document.querySelector("button");
+
     conte.addEventListener("click", function() {
         alert("Hola! Soy el div");
+    });
+
+    boton.addEventListener("click", function (){
+        alert('Hola!');
     });
 })
