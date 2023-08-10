eb58cef6572b3e39892a5ab10114595c330f41be
Mauricio Machado <133698730+machadokroyen@users.noreply.github.com>
2023-08-10 20:06:06 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(){
     let conte = document.querySelector("div");
-
+    let boton = document.querySelector("button");
+    
     conte.addEventListener("click", function() {
         alert("Hola! Soy el div");
+    });
+
+    boton.addEventListener("click", function (x){
+        alert('Hola!');
+        x.stopPropagation();
     });
 })
